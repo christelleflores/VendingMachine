71bb3adaaf0a55217120938f62ddead0ef1c7f72
christelle flores <57645973+christelleflores@users.noreply.github.com>
2021-02-12 20:53:12 -0500

    finalized money exchange

--- src/VendingMAchine.java
+++ src/VendingMAchine.java
@@ -16,36 +16,36 @@ public class VendingMAchine {
 				|| !snack.equals("5") || !snack.equals("6") || !snack.equals("7")) {
 			if (snack.equals("1")) {
 				total = total + 1;
-				System.out.println("You have selected 1. Snicker Bar.");
+				System.out.println("You have selected Snicker Bar.");
 				System.out.println("Your total is $1.");
 			} else if (snack.equals("2")) {
 				total = total + 2;
-				System.out.println("You have selected 2. Pop-Tarts.");
+				System.out.println("You have selected Pop-Tarts.");
 				System.out.println("Your total is $2.");
 				break;
 			} else if (snack.equals("3")) {
 				total = total + 2;
-				System.out.println("You have selected 3. Sun Chips.");
+				System.out.println("You have selected Sun Chips.");
 				System.out.println("Your total is $2.");
 				break;
 			} else if (snack.equals("4")) {
 				total = total + 2;
-				System.out.println("You have selected 4. Nacho Doritos.");
+				System.out.println("You have selected Nacho Doritos.");
 				System.out.println("Your total is $2.");
 				break;
 			} else if (snack.equals("5")) {
 				total = total + 1;
-				System.out.println("You have selected 5. Pretzels.");
+				System.out.println("You have selected Pretzels.");
 				System.out.println("Your total $1.");
 				break;
 			} else if (snack.equals("6")) {
 				total = total + 2;
-				System.out.println("You have selected 6. Cheetos.");
+				System.out.println("You have selected Cheetos.");
 				System.out.println("Your total is $2.");
 				break;
 			} else if (snack.equals("7")) {
 				total = total + 1;
-				System.out.println("You have selected 7. Popcorn.");
+				System.out.println("You have selected Popcorn.");
 				System.out.println("Your total is $1.");
 				break;
 			} else {
@@ -56,10 +56,12 @@ public class VendingMAchine {
 
 		System.out.println("How much money will you be inserting?");
 		int money = scanner.nextInt();
-		
+
 		if (money > total) {
 			int difference = money - total;
 			System.out.println("Your change is $" + difference + ". Enjoy your snack!");
-		} 
+		} else {
+			System.out.println("Thank you. Enjoy your snack!");
+		}
 	}
 }
