e5fc842ed79ed3473f3d8c49a08535f4ba1efcbb
Humna Hanif <humnahanif@Humnas-MacBook-Pro.local>
2021-02-19 18:19:00 -0500

    Enthusiasm

--- src/VendingMAchine.java
+++ src/VendingMAchine.java
@@ -4,7 +4,7 @@ public class VendingMAchine {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
+		
 		System.out.println("Welcome to our Vending Machine. Please select the snack number you wish to buy:");
 		System.out.println(
 				" 1. Snicker Bar \n 2. Pop-Tarts \n 3. Sun Chips \n 4. Nacho Doritos \n 5. Pretzels \n 6. Cheetos \n 7. Popcorn");
@@ -16,40 +16,40 @@ public class VendingMAchine {
 				|| !snack.equals("5") || !snack.equals("6") || !snack.equals("7")) {
 			if (snack.equals("1")) {
 				total = total + 1;
-				System.out.println("You have selected Snicker Bar.");
+				System.out.println("You have selected Snicker Bar!");
 				System.out.println("Your total is $1.");
 			} else if (snack.equals("2")) {
 				total = total + 2;
-				System.out.println("You have selected Pop-Tarts.");
+				System.out.println("You have selected Pop-Tarts!");
 				System.out.println("Your total is $2.");
 				break;
 			} else if (snack.equals("3")) {
 				total = total + 2;
-				System.out.println("You have selected Sun Chips.");
+				System.out.println("You have selected Sun Chips!");
 				System.out.println("Your total is $2.");
 				break;
 			} else if (snack.equals("4")) {
 				total = total + 2;
-				System.out.println("You have selected Nacho Doritos.");
+				System.out.println("You have selected Nacho Doritos!");
 				System.out.println("Your total is $2.");
 				break;
 			} else if (snack.equals("5")) {
 				total = total + 1;
-				System.out.println("You have selected Pretzels.");
+				System.out.println("You have selected Pretzels!");
 				System.out.println("Your total $1.");
 				break;
 			} else if (snack.equals("6")) {
 				total = total + 2;
-				System.out.println("You have selected Cheetos.");
+				System.out.println("You have selected Cheetos!");
 				System.out.println("Your total is $2.");
 				break;
 			} else if (snack.equals("7")) {
 				total = total + 1;
-				System.out.println("You have selected Popcorn.");
+				System.out.println("You have selected Popcorn!");
 				System.out.println("Your total is $1.");
 				break;
 			} else {
-				System.out.println("Invalid choice. Please enter a valid snack number.");
+				System.out.println("Invalid choice. Please enter a valid snack number!");
 				snack = scanner.nextLine();
 			}
 		}
